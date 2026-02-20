72c5e7ac4c6d4b4076e29f97dc987d033d130596
Dav10-32 <david.palacios-p@mail.escuelaing.edu.co>
2026-02-19 23:31:36 -0500

    Punto 2, base datos PostgreSQL

--- src/main/java/edu/eci/arsw/blueprints/model/Point.java
+++ src/main/java/edu/eci/arsw/blueprints/model/Point.java
@@ -1,3 +1,49 @@
 package edu.eci.arsw.blueprints.model;
 
-public record Point(int x, int y) { }
+import jakarta.persistence.Embeddable;
+import java.io.Serializable;
+
+@Embeddable
+public class Point implements Serializable {
+
+    private int x;
+    private int y;
+
+    public Point() {
+    }
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    // Compatibility methods for record-style access
+    public int x() {
+        return x;
+    }
+
+    public int y() {
+        return y;
+    }
+
+    @Override
+    public String toString() {
+        return "Point{" + "x=" + x + ", y=" + y + '}';
+    }
+}
